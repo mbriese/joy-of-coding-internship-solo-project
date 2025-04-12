20dc01d9a6cc66e83e62ea9c5313040d3f5887a1
MBriese <mindi.briese@gmail.com>
2025-04-11 18:02:04 -0700

    added user folder, user list, new user page fixed capitalization issues added task skeleton

---
+++ task-manager/app/tasks/new/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>make a new task</div>
+    )
+}
+export default Page
---
+++ task-manager/app/tasks/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <h1>Tasks</h1>
+    )
+}
+export default Page
