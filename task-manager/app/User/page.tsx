02b2a5e5f85acab85257af2d57fd354392cede19
MBriese <mindi.briese@gmail.com>
2025-04-11 17:32:50 -0700

    added user folder, user list, new user page

---
+++ task-manager/app/User/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddUser from "@/app/User/NewUser/AddUser";
+
+const Users = () => {
+    return (
+        <>
+        <div className="block bg-clip-padding">Users List</div>
+        < AddUser />
+        </>
+    )
+}
+export default Users
