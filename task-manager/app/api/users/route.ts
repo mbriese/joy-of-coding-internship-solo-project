00bed2bbced08754e400ab2d3d9f12d864006ea3
MBriese <mindi.briese@gmail.com>
2025-04-30 16:23:18 -0700

    setup radix-ui and fixed user/route.ts to remove error checking

--- task-manager/app/api/users/route.ts
+++ task-manager/app/api/users/route.ts
@@ -1,25 +1,23 @@
 import {NextRequest, NextResponse} from "next/server";
-//import { z } from "zod";
+import { z } from "zod";
 import {PrismaClient} from "../../generated/prisma/client"
 
 const prisma = new PrismaClient()
 
-// const createUserSchema = z.object({
-//     name: z.string().min(1).max(255),
-//     email: z.string().min(1),
-// })
+const createUserSchema = z.object({
+    name: z.string().min(1).max(255),
+    email: z.string().min(1),
+})
 
 export async function POST (request: NextRequest) {
-    console.log("tell me if I got to post user");
     const body = await request.json();
     console.log(JSON.stringify(body));
-  //  const validation = createUserSchema.safeParse(body);
+    
+    const validation = createUserSchema.safeParse(body);
 
-  //  if (!validation.success)
-  //      return NextResponse.json(validation.error.errors, {status: 400})
+     if (!validation.success)
+        return NextResponse.json(validation.error.errors, {status: 400})
 
-
-   // const newUser = await prisma.user.create({
     const newUser = await prisma.user.create({
         data: {name: body.name, email: body.email}
     });
