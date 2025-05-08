a78aeb92c63aca43be71ba50e98d74141c7d117f
MBriese <mindi.briese@gmail.com>
2025-05-07 19:55:25 -0700

    format new user and new task buttons customize radix-ui theme verifying client side validation clean up dashboard and api for tasks want to only select tasks for default user temporarily changed dashboard links to buttons

--- task-manager/Frontend/pages/Dashboard.tsx
+++ task-manager/Frontend/pages/Dashboard.tsx
@@ -13,14 +13,15 @@ const Dashboard = () => {
         {label: 'My Completed tasks List', href: '/tasks'}];
     // @ts-ignore
     return <>
-        <h1 className='text-2xl'>Welcome to your personal task manager!</h1>
-        <ul className="flex space-x-6">
+        <div className="col-start-3 row-start-3 flex max-w-lg flex-col space-y-6 font-bold
+         bg-gray-100 p-2 dark:bg-white/10">Welcome to your personal task manager!</div>
+        <ul className="flex -top-px -bot space-x-6">
             {links.map(link => (
                 <li key={link.href}>
                     <Link
                         href={link.href}
                         className={classnames(
-                            'px-4 py-2 rounded-md font-semibold transition-colors shadow-sm',
+                            'top-2 px-4 py-2 rounded-md font-semibold transition-colors shadow-sm',
                             {
                                 'bg-blue-600 text-white hover:bg-blue-700': link.href === currentPath,
                                 'bg-zinc-100 text-zinc-700 hover:bg-zinc-200': link.href !== currentPath,
