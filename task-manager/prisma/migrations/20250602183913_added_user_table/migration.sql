939c7b1bff7111c4086f5a92e41a67fef7431897
MBriese <mindi.briese@gmail.com>
2025-06-02 13:19:47 -0700

    fixed database issues

---
+++ task-manager/prisma/migrations/20250602183913_added_user_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fname` VARCHAR(255) NOT NULL,
+    `lname` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
